53b63694f80f2964d07c76c1e8fbd819e6af69ef
Jan Król <jk.jankrol@gmail.com>
2024-04-26 14:51:24 +0200

    Adjust patterns  parsing in cli

--- cli.py
+++ cli.py
@@ -1,10 +1,11 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, ArgumentTypeError, Namespace
 
 
 def create_cli_parser() -> ArgumentParser:
     parser = ArgumentParser(description="Python implementation of grep command")
-    parser.add_argument("-e", "--pattern", action="append", dest="patterns", help="Pattern(s) to search for")
+    parser.add_argument("pattern", help="Pattern to search for in the file(s).")
     parser.add_argument("files", nargs='+', help="Files to search in")
+    parser.add_argument("-e", "--pattern", action="append", dest="patterns", help="Additional pattern(s) to search for")
     parser.add_argument("-c", "--count", action="store_true",
                         help="prints only a count of selected lines per file")
     parser.add_argument("-r", "--recursive", action="store_true",
@@ -18,3 +19,14 @@ def create_cli_parser() -> ArgumentParser:
     parser.add_argument("-n", "--line-number", action="store_true",
                         help="print line number with output lines")
     return parser
+
+
+def merge_pattern_related_args(args: Namespace) -> Namespace:
+    if args.pattern is None and args.patterns is None:
+        raise ArgumentTypeError("Either -e option or pattern argument is required.")
+    elif args.patterns is None:
+        args.patterns = [args.pattern]
+    else:
+        args.files.insert(0, args.pattern)
+
+    return args
--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from typing import List, Optional
 
-from cli import create_cli_parser
+from cli import create_cli_parser, merge_pattern_related_args
 from command.grep.factory import create_grep_from_cli_args
 
 
 def main(args: Optional[List[str]] = None) -> None:
-    test = "super interesting data x2"
     cli_parser = create_cli_parser()
-    parsed_args = cli_parser.parse_args(args)
+    parsed_args = merge_pattern_related_args(cli_parser.parse_args(args))
     grep = create_grep_from_cli_args(parsed_args)
     grep.execute()
 
